14b72cd0dbb6652487241521f0cbeedf7b097ef8
Ian Macaulay <ismacaul@gmail.com>
2020-08-27 21:21:21 -0600

    Refactored into better cli command handling

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,8 +1,126 @@
-pub mod list {
-    use crate::{db, input, models};
+use crate::db;
+use crate::CommandCtx;
 
-    pub fn description() -> &'static str {
-        "Manage lists"
+pub fn list(_: &CommandCtx) {
+    let lists = db::get_lists();
+
+    println!("ID\tNAME\tDESCRIPTION");
+    for l in lists.iter() {
+        println!("{}\t{}\t{}", l.id, l.title, l.description);
+    }
+}
+
+pub mod list {
+    use crate::{db, input, models, CommandCtx};
+
+    pub fn create(ctx: &CommandCtx) {
+        let mut title: Option<String> = None;
+        let mut description: Option<String> = None;
+
+        if let Some(value) = ctx.data.get("title") {
+            title = Some(String::from(value));
+        }
+
+        if let Some(value) = ctx.data.get("desc") {
+            description = Some(String::from(value));
+        }
+
+        if title == None && description == None {
+            // get input from file
+            let text = input::get_file_input(None);
+            if let Some(result) = split_text_into_title_desc(&text) {
+                let (t, d) = result;
+                title = t;
+                description = d;
+            }
+        } else if title == None && description != None {
+            // get input for title from stdin
+            print!("Please enter the list title: ");
+            let text = input::get_stdin_input();
+            if text.len() > 0 {
+                title = Some(text);
+            }
+        } else if title != None && description == None {
+            // save title with empty description
+            description = Some(String::from(""));
+        }
+
+        if title == None {
+            println!("Aborting: no list title");
+            std::process::exit(1);
+        }
+
+        db::create_list(&title.unwrap(), &description.unwrap());
+    }
+
+    pub fn edit(ctx: &CommandCtx) {
+        if ctx.params.len() == 0 {
+            // TODO: use current list and show editor
+        } else if ctx.params.len() == 1 {
+            let list_id = &ctx.params[0];
+            if let Some(list) = find_list(list_id).as_mut() {
+                let mut title: Option<String> = None;
+                let mut description: Option<String> = None;
+
+                if let Some(value) = ctx.data.get("title") {
+                    title = Some(String::from(value));
+                }
+
+                if let Some(value) = ctx.data.get("desc") {
+                    description = Some(String::from(value));
+                }
+
+                if title == None && description == None {
+                    // get input from file
+                    let current = vec![list.title.clone(), list.description.clone()].join("\n");
+                    let text = input::get_file_input(Some(&current));
+                    if let Some(result) = split_text_into_title_desc(&text) {
+                        let (t, d) = result;
+                        title = t;
+                        description = d;
+                    }
+                }
+
+                if title.is_some() || description.is_some() {
+                    if let Some(t) = title {
+                        list.title = t;
+                    }
+
+                    if let Some(d) = description {
+                        list.description = d;
+                    }
+
+                    db::update_list(list);
+                }
+            } else {
+                println!("Aborting: Could not find list '{}'", list_id)
+            }
+        }
+    }
+
+    pub fn delete(ctx: &CommandCtx) {
+        if ctx.params.len() == 0 {
+            // TODO: use current list
+        } else {
+            for p in ctx.params.iter() {
+                if let Some(list) = find_list(p) {
+                    println!("Are you sure you want to delete list '{}'?", list.title);
+                    println!("This cannot be undone!");
+                    print!("Enter ther name of the list to confirm: ");
+                    let result = input::get_stdin_input();
+                    if result == list.title {
+                        db::delete_list(&list);
+                    } else {
+                        println!(
+                            "Skipping '{}'. Entered title does not match {}",
+                            p, list.title
+                        );
+                    }
+                } else {
+                    println!("Skipping '{}'. Not found!", p);
+                }
+            }
+        }
     }
 
     fn split_text_into_title_desc(text: &String) -> Option<(Option<String>, Option<String>)> {
@@ -19,231 +137,72 @@ pub mod list {
         return None;
     }
 
-    pub fn process(args: &[String]) {
-        if args.len() == 0 {
-            let lists = db::get_lists();
-
-            println!("ID\tNAME\tDESCRIPTION");
-            for l in lists.iter() {
-                println!("{}\t{}\t{}", l.id, l.title, l.description);
-            }
-            return;
+    fn find_list(list_id: &String) -> Option<models::List> {
+        let list = db::find_list_by_title(list_id);
+        if list.is_some() {
+            return list;
         }
 
-        match args[0].as_str() {
-            "create" => {
-                let mut title: Option<String> = None;
-                let mut description: Option<String> = None;
-
-                if args.len() == 1 {
-                    let text = input::get_file_input(None);
-                    if let Some(result) = split_text_into_title_desc(&text) {
-                        let (t, d) = result;
-                        title = t;
-                        description = d;
-                    }
-                } else {
-                    let (t, d) = parse_name_and_description_options(&args[1..]);
-                    title = t;
-                    description = d;
-                }
-
-                if title == None {
-                    println!("Aborting: no list name");
-                } else {
-                    if description == None {
-                        db::create_list(&title.unwrap(), &String::from(""));
-                    } else {
-                        db::create_list(&title.unwrap(), &description.unwrap());
-                    }
-                }
-            }
-            "edit" => {
-                if args.len() == 1 {
-                    // TODO: use current list and show editor
-                } else if args.len() == 2 {
-                    let list_id = &args[1];
-
-                    if let Some(list) = db::find_list_by_title(list_id).as_mut() {
-                        edit_list(list);
-                    } else if let Some(list) = db::find_list_by_id(list_id).as_mut() {
-                        edit_list(list);
-                    } else {
-                        println!("Not Found: {:?}", list_id);
-                    }
-                } else {
-                    let list_id = &args[1];
-
-                    if let Some(list) = db::find_list_by_title(list_id).as_mut() {
-                        let (title, description) = parse_name_and_description_options(&args[2..]);
-                        update_list_with_title_and_description(list, title, description);
-                    } else if let Some(list) = db::find_list_by_id(list_id).as_mut() {
-                        let (title, description) = parse_name_and_description_options(&args[2..]);
-                        update_list_with_title_and_description(list, title, description);
-                    } else {
-                        println!("Not Found: {:?}", list_id);
-                    }
-                }
-            }
-            "delete" => {
-                if args.len() == 1 {
-                    // TODO: use current list
-                } else if args.len() == 2 {
-                    let list_id = &args[1];
-
-                    if let Some(list) = db::find_list_by_title(list_id).as_mut() {
-                        delete_list(list);
-                    } else if let Some(list) = db::find_list_by_id(list_id).as_mut() {
-                        delete_list(list);
-                    } else {
-                        println!("Not Found: {:?}", list_id);
-                    }
-                }
-            }
-            "show" => {}
-            "help" | "--help" | "-h" => print_help_and_exit(0),
-            _ => print_help_and_exit(1),
-        }
+        return db::find_list_by_id(list_id);
     }
-
-    fn print_help_and_exit(code: i32) {
-        println!("procrast list COMMAND");
-        std::process::exit(code);
-    }
-
-    fn edit_list(list: &mut models::List) {
-        let current = vec![list.title.clone(), list.description.clone()].join("\n");
-        let update = input::get_file_input(Some(&current));
-        if let Some(result) = split_text_into_title_desc(&update) {
-            let (title, mut description) = result;
-
-            if description == None {
-                description = Some(String::from(""));
-            }
-
-            update_list_with_title_and_description(list, title, description);
-        }
-    }
-
-    fn delete_list(list: &models::List) {
-        println!("Are you sure you want to delete list '{}'?", list.title);
-        println!("This cannot be undone!");
-        print!("Enter ther name of the list to confirm:");
-        let result = input::get_stdin_input();
-        if result == list.title {
-            db::delete_list(list);
-        } else {
-            println!("Aborting: Entered name does not match {}", list.title);
-        }
-    }
-
-    fn update_list_with_title_and_description(
-        list: &mut models::List,
-        title: Option<String>,
-        description: Option<String>,
-    ) {
-        if let Some(t) = title {
-            list.title = t;
-        }
-
-        if let Some(d) = description {
-            list.description = d;
-        }
-
-        db::update_list(list)
-    }
-
-    fn parse_name_and_description_options(args: &[String]) -> (Option<String>, Option<String>) {
-        let mut idx = 0;
-        let mut title: Option<String> = None;
-        let mut description: Option<String> = None;
-
-        while idx < args.len() {
-            match args[idx].as_str() {
-                "-n" => {
-                    idx += 1;
-
-                    if idx < args.len() {
-                        title = Some(args[idx].clone());
-                        idx += 1;
-                    } else {
-                        print_help_and_exit(1);
-                    }
-                }
-                "-d" => {
-                    idx += 1;
-
-                    if idx < args.len() {
-                        description = Some(args[idx].clone());
-                        idx += 1;
-                    } else {
-                        print_help_and_exit(1);
-                    }
-                }
-                _ => {
-                    println!("Unknown option '{}'", args[idx]);
-                    print_help_and_exit(1);
-                }
-            }
-        }
-
-        return (title, description);
-    }
+    //     pub fn process(args: &[String]) {
+    //         match args[0].as_str() {
+    //             "delete" => {
+    //                 if args.len() == 1 {
+    //                     // TODO: use current list
+    //                 } else if args.len() == 2 {
+    //                     let list_id = &args[1];
+    //
+    //                     if let Some(list) = db::find_list_by_title(list_id).as_mut() {
+    //                         delete_list(list);
+    //                     } else if let Some(list) = db::find_list_by_id(list_id).as_mut() {
+    //                         delete_list(list);
+    //                     } else {
+    //                         println!("Not Found: {:?}", list_id);
+    //                     }
+    //                 }
+    //             }
+    //             "show" => {}
+    //             "help" | "--help" | "-h" => print_help_and_exit(0),
+    //             _ => print_help_and_exit(1),
+    //         }
+    //     }
+    //
+    //
+    //     fn delete_list(list: &models::List) {
+    //         println!("Are you sure you want to delete list '{}'?", list.title);
+    //         println!("This cannot be undone!");
+    //         print!("Enter ther name of the list to confirm:");
+    //         let result = input::get_stdin_input();
+    //         if result == list.title {
+    //             db::delete_list(list);
+    //         } else {
+    //             println!("Aborting: Entered name does not match {}", list.title);
+    //         }
+    //     }
 }
 
 pub mod item {
-    pub fn description() -> &'static str {
-        "Manage items"
-    }
-
-    pub fn process(args: &[String]) {
-        if args.len() == 0 {
-            print_help_and_exit(1);
-        }
-
-        match args[0].as_str() {
-            "add" => {}
-            "edit" => {}
-            "delete" => {}
-            "help" => print_help_and_exit(0),
-            _ => print_help_and_exit(1),
-        }
-    }
-
-    fn print_help_and_exit(code: i32) {
-        println!("procrast item COMMAND");
-        std::process::exit(code);
-    }
-}
-
-fn print_help_and_exit(code: i32) {
-    println!(
-        "
-
-Usage: procrast COMMAND
-
-A cli for managing your procrastination
-
-Commands:
-  list  {}
-  item  {}
-",
-        list::description(),
-        item::description()
-    );
-
-    std::process::exit(code);
-}
-
-pub fn process(args: &[String]) {
-    if args.len() == 0 {
-        print_help_and_exit(1);
-    }
-
-    match args[0].as_str() {
-        "list" => list::process(&args[1..]),
-        "item" => item::process(&args[1..]),
-        "help" | "--help" | "-h" => print_help_and_exit(0),
-        _ => print_help_and_exit(1),
-    };
+    // pub fn description() -> &'static str {
+    //     "Manage items"
+    // }
+    //
+    // pub fn process(args: &[String]) {
+    //     if args.len() == 0 {
+    //         print_help_and_exit(1);
+    //     }
+    //
+    //     match args[0].as_str() {
+    //         "add" => {}
+    //         "edit" => {}
+    //         "delete" => {}
+    //         "help" => print_help_and_exit(0),
+    //         _ => print_help_and_exit(1),
+    //     }
+    // }
+    //
+    // fn print_help_and_exit(code: i32) {
+    //     println!("procrast item COMMAND");
+    //     std::process::exit(code);
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,280 @@ mod db;
 mod input;
 mod models;
 
+use std::collections::HashMap;
 use std::env;
 
+enum CommandParams {
+    None,
+    Single(&'static str),
+    Multi(&'static str),
+}
+
+struct Flag {
+    name: &'static str,
+    // TODO: change aliases to short
+    aliases: Vec<&'static str>,
+    description: &'static str,
+}
+
+pub struct CommandCtx {
+    // TODO rename data to options
+    data: HashMap<&'static str, String>,
+    params: Vec<String>,
+}
+
+impl CommandCtx {
+    fn new() -> CommandCtx {
+        CommandCtx {
+            data: HashMap::new(),
+            params: Vec::new(),
+        }
+    }
+}
+
+struct Command {
+    name: &'static str,
+    aliases: Vec<&'static str>,
+    description: &'static str,
+    params: CommandParams,
+    action: fn(ctx: &CommandCtx),
+    subcommands: Vec<Command>,
+    flags: Vec<Flag>,
+}
+
+impl Command {
+    fn run(&self, args: &[String]) {
+        if args.len() == 0 {
+            let ctx = CommandCtx::new();
+            (self.action)(&ctx);
+        } else {
+            let cmd = args[0].as_str();
+            match cmd {
+                "help" | "--help" | "-h" => self.print_help_and_exit(0),
+                _ => {
+                    if let Some(command) = self
+                        .subcommands
+                        .iter()
+                        .find(|c| c.name == cmd || c.aliases.iter().find(|a| **a == cmd) != None)
+                    {
+                        command.run(&args[1..]);
+                    } else {
+                        let mut ctx = CommandCtx::new();
+                        let mut arg_iter = args.iter();
+
+                        while let Some(a) = arg_iter.next() {
+                            if a.starts_with("--") {
+                                if let Some(flag) = self
+                                    .flags
+                                    .iter()
+                                    .find(|f| f.name == a.strip_prefix("--").unwrap())
+                                {
+                                    if let Some(v) = arg_iter.next() {
+                                        ctx.data.insert(flag.name, v.to_string());
+                                    } else {
+                                        println!(
+                                            "Aborting: flag '{}' is missing the value",
+                                            flag.name
+                                        );
+                                        self.print_help_and_exit(1);
+                                    }
+                                }
+                            } else if a.starts_with("-") {
+                                if let Some(flag) = self.flags.iter().find(|f| {
+                                    f.aliases
+                                        .iter()
+                                        .find(|fa| **fa == a.strip_prefix("-").unwrap())
+                                        != None
+                                }) {
+                                    if let Some(v) = arg_iter.next() {
+                                        ctx.data.insert(flag.name, v.to_string());
+                                    } else {
+                                        println!(
+                                            "Aborting: flag '{}' is missing the value",
+                                            flag.name
+                                        );
+                                        self.print_help_and_exit(1);
+                                    }
+                                }
+                            } else {
+                                ctx.params.push(a.to_string());
+                            }
+                        }
+
+                        (self.action)(&ctx);
+                    }
+                }
+            };
+        }
+    }
+
+    fn print_help_and_exit(&self, code: i32) {
+        let mut help_str = String::with_capacity(20);
+        help_str.push_str(&format!("\nUsage: {}", self.name));
+
+        if self.subcommands.len() > 0 {
+            help_str.push_str(&format!(" COMMAND"));
+        }
+
+        if self.flags.len() > 0 {
+            help_str.push_str(&format!(" OPTIONS"));
+        }
+
+        match self.params {
+            CommandParams::Single(name) => help_str.push_str(&format!(" {}", name)),
+            CommandParams::Multi(name) => help_str.push_str(&format!(" {0} [{0}...]", name)),
+            _ => {}
+        }
+
+        help_str.push_str(&format!("\n\n{}\n", self.description));
+
+        if self.subcommands.len() > 0 {
+            help_str.push_str("\nCommands:");
+
+            for c in self.subcommands.iter() {
+                help_str.push_str(&format!("\n  {}\t{}", c.name, c.description));
+            }
+        }
+
+        if self.flags.len() > 0 {
+            help_str.push_str("\nOptions:");
+
+            for f in self.flags.iter() {
+                let mut alias_str = String::with_capacity(f.aliases.len() * 2);
+                if f.aliases.len() > 0 {
+                    for a in f.aliases.iter() {
+                        alias_str.push_str(&format!("-{},", a));
+                    }
+                }
+                help_str.push_str(&format!(
+                    "\n  {} --{} string\t{}",
+                    alias_str, f.name, f.description
+                ));
+            }
+        }
+
+        println!("{}", help_str);
+        std::process::exit(code);
+    }
+}
+
+struct Cli {
+    name: &'static str,
+    description: &'static str,
+    commands: Vec<Command>,
+}
+
+impl Cli {
+    fn run(&self, args: &[String]) {
+        if args.len() == 0 {
+            self.print_help_and_exit(1);
+        }
+
+        let cmd = args[0].as_str();
+        match cmd {
+            "help" | "--help" | "-h" => self.print_help_and_exit(0),
+            _ => {
+                if let Some(command) = self
+                    .commands
+                    .iter()
+                    .find(|c| c.name == cmd || c.aliases.iter().find(|a| **a == cmd) != None)
+                {
+                    command.run(&args[1..]);
+                } else {
+                    self.print_help_and_exit(1);
+                }
+            }
+        };
+    }
+
+    fn print_help_and_exit(&self, code: i32) {
+        let mut command_str = String::with_capacity(20);
+        for c in self.commands.iter() {
+            let s = format!("  {}  {}\n", c.name, c.description);
+            command_str.push_str(&s);
+        }
+
+        println!(
+            "
+
+Usage: {} COMMAND
+
+{}
+
+Commands:
+{}",
+            self.name, self.description, command_str
+        );
+
+        std::process::exit(code);
+    }
+}
+
 fn main() {
+    let app = Cli {
+        name: "procrast",
+        description: "A cli for managing your procrastination",
+        commands: vec![Command {
+            name: "list",
+            aliases: vec!["l"],
+            description: "manage lists",
+            params: CommandParams::None,
+            action: cmd::list,
+            flags: vec![],
+            subcommands: vec![
+                Command {
+                    name: "create",
+                    aliases: vec!["c"],
+                    description: "create a new list",
+                    params: CommandParams::None,
+                    action: cmd::list::create,
+                    subcommands: vec![],
+                    flags: vec![
+                        Flag {
+                            name: "title",
+                            aliases: vec!["t"],
+                            description: "the list title",
+                        },
+                        Flag {
+                            name: "desc",
+                            aliases: vec!["d"],
+                            description: "the list description",
+                        },
+                    ],
+                },
+                Command {
+                    name: "edit",
+                    aliases: vec!["e"],
+                    description: "Edit an existing list",
+                    params: CommandParams::Single("LIST"),
+                    action: cmd::list::edit,
+                    subcommands: vec![],
+                    flags: vec![
+                        Flag {
+                            name: "title",
+                            aliases: vec!["t"],
+                            description: "the list title",
+                        },
+                        Flag {
+                            name: "desc",
+                            aliases: vec!["d"],
+                            description: "the list description",
+                        },
+                    ],
+                },
+                Command {
+                    name: "delete",
+                    aliases: vec!["d"],
+                    description: "Delete one or more lists",
+                    params: CommandParams::Multi("LIST"),
+                    action: cmd::list::delete,
+                    subcommands: vec![],
+                    flags: vec![],
+                },
+            ],
+        }],
+    };
+
     let args: Vec<String> = env::args().collect();
-    cmd::process(&args[1..]);
+    app.run(&args[1..]);
 }
